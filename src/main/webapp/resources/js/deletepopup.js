fbdf3dcff16c91832cccbee3da4645c3658a7c0c
K.Aoki <kanako@keAokinoMacBook-Air.local>
2021-05-24 10:38:40 +0900

    指摘部分を修正いたしました

--- src/main/webapp/resources/js/deletepopup.js
+++ src/main/webapp/resources/js/deletepopup.js
@@ -4,9 +4,6 @@ $(function(){
  	if(!confirm('本当に削除しますか？')){
         /* キャンセルの時の処理 */
         return false;
-    }else{
-        /*　OKの時の処理 */
-        location.href = 'index.html';
     }
 });
 	});
